c3dd756a2d5385d37994c8098be725121520b32c
Anderson <BMAnderson@mdanderson.org>
2020-07-27 09:09:46 -0500

    only look in the 'correct' folder, only run if 'New_RT' doesn't exist, and ensure contours are present

--- Main.py
+++ Main.py
@@ -6,12 +6,15 @@ from Dicom_RT_and_Images_to_Mask.Image_Array_And_Mask_From_Dicom_RT import Dicom
 
 
 def convert_rts(base_path):
-    for path, directory, files in os.walk(base_path):  # Look through all folders
+    for path, directories, files in os.walk(base_path):  # Look through all folders
         files = [i for i in files if i.endswith('.dcm')]  # Stop if we have something with dicom
-        if files and len(files) > 5:  # Only go forward if we have at least 5 images
+        if files and len(files) > 5 and 'New_RT' not in directories:  # Only go forward if we have at least 5 images
             base_reader = Dicom_to_Imagestack(get_images_mask=False)  # Make a base reader to identify the contour names
             base_reader.down_folder(path)
             Contour_Names = base_reader.rois_in_case  # Keep the same nomenclature for naming
+            if not Contour_Names:
+                print('No contours found at {}'.format(path))
+                continue
             reader = Dicom_to_Imagestack(get_images_mask=True,
                                          Contour_Names=Contour_Names,
                                          arg_max=False)  # Now, create a new reader to create a mask of the contours
@@ -22,4 +25,4 @@ def convert_rts(base_path):
 
 
 if __name__ == '__main__':
-    convert_rts(base_path=r'K:\Morfeus\YHe\Brian')
+    convert_rts(base_path=r'K:\Morfeus\YHe\Brian\correct')
